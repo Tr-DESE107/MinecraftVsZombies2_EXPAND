58392da0aa02c13dfbf6ed3c13b2665558e497b6
Cuerzor58 <773785114@qq.com>
2024-09-10 22:41:54 +0800

    加入主界面预制件。

---
+++ Assets/Scripts/MVZ2/Localization/TextMeshProTranslator.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace MVZ2.Localization
+{
+    [RequireComponent(typeof(TextMeshPro))]
+    public class TextMeshProTranslator : TranslateComponent<TextMeshPro>
+    {
+        protected override string GetKeyInner()
+        {
+            return Component.text;
+        }
+        protected override void Translate(string language)
+        {
+            base.Translate(language);
+            if (!string.IsNullOrEmpty(Context))
+                Component.text = lang._p(Context, Key);
+            else
+                Component.text = lang._(Key);
+        }
+    }
+}
