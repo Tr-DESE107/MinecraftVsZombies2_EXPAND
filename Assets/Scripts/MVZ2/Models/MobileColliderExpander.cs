1cac045911ea7498be2bf89becc449feeed3823f
Cuerzor58 <773785114@qq.com>
2025-01-13 22:13:45 +0800

    移动端的掉落物触碰范围变为2倍。

---
+++ Assets/Scripts/MVZ2/Models/MobileColliderExpander.cs
@@ -0,0 +1,61 @@
+﻿using MVZ2.Managers;
+using UnityEngine;
+
+namespace MVZ2.Models
+{
+    public class MobileColliderExpander : MonoBehaviour
+    {
+        private void Update()
+        {
+            var shouldActive = MainManager.Instance.IsMobile();
+            if (active != shouldActive)
+            {
+                if (shouldActive)
+                {
+                    Enable();
+                }
+                else
+                {
+                    Disable();
+                }
+            }
+        }
+        private void OnDisable()
+        {
+            Disable();
+        }
+        private void Enable()
+        {
+            if (active)
+                return;
+            active = true;
+            var collider = GetComponent<Collider2D>();
+            switch (collider)
+            {
+                case CircleCollider2D circle:
+                    circle.radius *= 2;
+                    break;
+                case BoxCollider2D box:
+                    box.size *= 2;
+                    break;
+            }
+        }
+        private void Disable()
+        {
+            if (!active)
+                return;
+            active = false;
+            var collider = GetComponent<Collider2D>();
+            switch (collider)
+            {
+                case CircleCollider2D circle:
+                    circle.radius *= 0.5f;
+                    break;
+                case BoxCollider2D box:
+                    box.size *= 0.5f;
+                    break;
+            }
+        }
+        private bool active;
+    }
+}
