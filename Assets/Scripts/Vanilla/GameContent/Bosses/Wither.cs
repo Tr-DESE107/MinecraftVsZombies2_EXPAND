b14be437cce2b0395f51458d554d5d3ce2087420
Cuerzor58 <773785114@qq.com>
2025-02-24 18:38:48 +0800

    凋灵现在每秒恢复30生命。

--- Assets/Scripts/Vanilla/GameContent/Bosses/Wither.cs
+++ Assets/Scripts/Vanilla/GameContent/Bosses/Wither.cs
@@ -378,7 +378,7 @@ namespace MVZ2.GameContent.Bosses
         public const int HEAD_COUNT = 3;
         public const float HEAD_ROTATE_SPEED = 10;
         public const float FLY_HEIGHT = 80;
-        public const float REGENERATION_SPEED = 1 / 3f;
+        public const float REGENERATION_SPEED = 1;
         public const float EAT_HEALING = 600;
         public const float GOLDEN_APPLE_DAMAGE = 600;
         #endregion 常量
