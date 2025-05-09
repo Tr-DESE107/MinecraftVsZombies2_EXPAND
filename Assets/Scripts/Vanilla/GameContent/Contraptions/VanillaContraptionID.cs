5148cba96e2df50799dac1185161f9015c0832e3
Tr-DESE107 <1992679686@qq.com>
2025-05-10 04:49:34 +0800

    加入了第四难度的底座，蜘蛛女王死亡动画完成，更改了图鉴部分内容，初步新增了发光的黑曜石

--- Assets/Scripts/Vanilla/GameContent/Contraptions/VanillaContraptionID.cs
+++ Assets/Scripts/Vanilla/GameContent/Contraptions/VanillaContraptionID.cs
@@ -10,6 +10,8 @@ namespace MVZ2.GameContent.Contraptions
         public const string obsidian = "obsidian";
         public const string mineTNT = "mine_tnt";
 
+        public const string GlowingObsidian = "GlowingObsidian";
+
         public const string smallDispenser = "small_dispenser";
         public const string moonlightSensor = "moonlight_sensor";
         public const string glowstone = "glowstone";
@@ -53,6 +55,8 @@ namespace MVZ2.GameContent.Contraptions
         public static readonly NamespaceID furnace = Get(VanillaContraptionNames.furnace);
         public static readonly NamespaceID obsidian = Get(VanillaContraptionNames.obsidian);
         public static readonly NamespaceID mineTNT = Get(VanillaContraptionNames.mineTNT);
+
+        public static readonly NamespaceID GlowingObsidian = Get(VanillaContraptionNames.GlowingObsidian);
 
         public static readonly NamespaceID smallDispenser = Get(VanillaContraptionNames.smallDispenser);
         public static readonly NamespaceID moonlightSensor = Get(VanillaContraptionNames.moonlightSensor);
