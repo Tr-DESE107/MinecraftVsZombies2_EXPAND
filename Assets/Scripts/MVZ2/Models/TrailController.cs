d338bf8e735ae2b523586cf8f3e5862d2e14b9ff
Cuerzor58 <773785114@qq.com>
2025-05-22 15:12:53 +0800

    修复恶魂火球的拖尾不会受缩放影响的问题。

--- Assets/Scripts/MVZ2/Models/TrailController.cs
+++ Assets/Scripts/MVZ2/Models/TrailController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace MVZ2.Models
 {
@@ -7,37 +8,70 @@ namespace MVZ2.Models
         public void Init()
         {
             trailPoints = new Vector3[trail.positionCount];
+            worldPositions = new Vector3[trail.positionCount];
+            defaultWidthMultiplier = trail.widthMultiplier;
             trail.GetPositions(trailPoints);
-            for (int i = 0; i < trailPoints.Length; i++)
+            for (int i = 0; i < worldPositions.Length; i++)
             {
-                trailPoints[i] = transform.position;
+                worldPositions[i] = GetCurrentWorldPosition();
+                trailPoints[i] = ToTrailPosition(worldPositions[i]);
             }
             trail.SetPositions(trailPoints);
         }
         public void UpdateLogic()
         {
             trail.GetPositions(trailPoints);
-            for (int i = trailPoints.Length - 1; i >= 0; i--)
+            for (int i = worldPositions.Length - 1; i >= 0; i--)
             {
+                Vector3 position;
                 if (i == 0)
                 {
-                    trailPoints[i] = transform.position;
+                    position = GetCurrentWorldPosition();
                 }
                 else
                 {
-                    trailPoints[i] = trailPoints[i - 1] + velocity * i;
+                    position = worldPositions[i - 1] + velocity * i;
                 }
+                worldPositions[i] = position;
+                trailPoints[i] = ToTrailPosition(position);
             }
             trail.SetPositions(trailPoints);
         }
         public void UpdateFrame()
         {
-            trail.SetPosition(0, transform.position);
+            var worldPos = GetCurrentWorldPosition();
+            var position = ToTrailPosition(worldPos);
+            trail.SetPosition(0, position);
+            if (scaleWithHierarchy)
+            {
+                var scale = transform.lossyScale;
+                trail.widthMultiplier = Mathf.Min(scale.x, scale.y) * defaultWidthMultiplier;
+            }
+            else
+            {
+                trail.widthMultiplier = defaultWidthMultiplier;
+            }
+        }
+        private Vector3 ToTrailPosition(Vector3 worldPos)
+        {
+            if (!trail.useWorldSpace)
+            {
+                return transform.InverseTransformPoint(worldPos);
+            }
+            return worldPos;
+        }
+        private Vector3 GetCurrentWorldPosition()
+        {
+            return transform.position;
         }
         [SerializeField]
         private LineRenderer trail;
         [SerializeField]
         private Vector3 velocity;
+        [SerializeField]
+        private bool scaleWithHierarchy;
+        private float defaultWidthMultiplier;
         private Vector3[] trailPoints = null;
+        private Vector3[] worldPositions = null;
     }
 }
