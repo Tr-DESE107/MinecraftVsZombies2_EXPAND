6d1b2f53ee0ac14921fdfe6741faa2dd96d38140
Cuerzor58 <773785114@qq.com>
2025-05-25 15:08:17 +0800

    修复岸边的僵尸受到重力板影响时，会沉入水底但不会死亡的问题。

--- Assets/Scripts/Vanilla/Modifiers/VanillaModifierPriorities.cs
+++ Assets/Scripts/Vanilla/Modifiers/VanillaModifierPriorities.cs
@@ -3,6 +3,7 @@
     public static class VanillaModifierPriorities
     {
         public const int GRAVITY_PAD = 900;
+        public const int WATER_GRAVITY = 901;
         public const int FORCE = 1000;
     }
 }
