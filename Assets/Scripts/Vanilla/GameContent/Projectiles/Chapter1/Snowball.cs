9b8a2a4fd3bdfa014c4e69999d1f4818519e2a82
Cuerzor58 <773785114@qq.com>
2025-06-23 21:15:47 +0800

    地狱火现在可以与雪球、大雪球、木球、冰锥发生反应。

--- Assets/Scripts/Vanilla/GameContent/Projectiles/Chapter1/Snowball.cs
+++ Assets/Scripts/Vanilla/GameContent/Projectiles/Chapter1/Snowball.cs
@@ -1,13 +1,19 @@
-﻿using MVZ2.Vanilla.Entities;
+﻿using MVZ2.GameContent.Contraptions;
+using MVZ2.Vanilla.Entities;
+using PVZEngine.Entities;
 using PVZEngine.Level;
 
 namespace MVZ2.GameContent.Projectiles
 {
     [EntityBehaviourDefinition(VanillaProjectileNames.snowball)]
-    public class Snowball : ProjectileBehaviour
+    public class Snowball : ProjectileBehaviour, IHellfireIgniteBehaviour
     {
         public Snowball(string nsp, string name) : base(nsp, name)
         {
+        }
+        public void Ignite(Entity entity, Entity hellfire, bool cursed)
+        {
+            entity.SetModelProperty("Melted", true);
         }
     }
 }
