8682a80a5aafb4ff466dc9d90d7f3bf80725aacc
Cuerzor58 <773785114@qq.com>
2025-06-29 12:02:48 +0800

    调整了“准备放置器械”文字的动画。

--- Assets/Scripts/Vanilla/GameContent/Audios/VanillaSoundID.cs
+++ Assets/Scripts/Vanilla/GameContent/Audios/VanillaSoundID.cs
@@ -142,6 +142,7 @@ namespace MVZ2.Vanilla.Audios
         public readonly static NamespaceID powerOn = Get("power_on");
         public readonly static NamespaceID punch = Get("punch");
         public readonly static NamespaceID rain = Get("rain");
+        public readonly static NamespaceID readySetBuild = Get("ready_set_build");
         public readonly static NamespaceID reflection = Get("reflection");
         public readonly static NamespaceID refuel = Get("refuel");
         public readonly static NamespaceID reviveCast = Get("revive_cast");
