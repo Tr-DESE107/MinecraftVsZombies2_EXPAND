e02db7181060c960c49570512534f363c9da3e16
Cuerzor58 <773785114@qq.com>
2025-07-01 20:23:37 +0800

    测试用分支。主界面按下F1即可游玩。

--- Assets/Scripts/MVZ2/Mainmenu/MainmenuController.cs
+++ Assets/Scripts/MVZ2/Mainmenu/MainmenuController.cs
@@ -477,7 +477,7 @@ namespace MVZ2.Mainmenu
             var task = main.LevelManager.GotoLevelSceneAsync();
             while (!task.IsCompleted)
                 yield return null;
-            main.LevelManager.InitLevel(VanillaAreaID.ship, VanillaStageID.debug);
+            main.LevelManager.InitLevel(VanillaAreaID.ship, VanillaStageID.halloween1);
             Hide();
         }
 
