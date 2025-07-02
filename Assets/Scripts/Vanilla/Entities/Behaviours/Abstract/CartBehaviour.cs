6625fddb28d9885b780e6faab3946d2c58484763
Cuerzor58 <773785114@qq.com>
2025-07-02 19:18:31 +0800

    添加了云山之拳击中怪物时的效果。

--- Assets/Scripts/Vanilla/Entities/Behaviours/Abstract/CartBehaviour.cs
+++ Assets/Scripts/Vanilla/Entities/Behaviours/Abstract/CartBehaviour.cs
@@ -2,12 +2,20 @@ using PVZEngine.Entities;
 
 namespace MVZ2.Vanilla.Entities
 {
-    public abstract class CartBehaviour : EntityBehaviourDefinition
+    public interface ICartBehaviour
+    {
+        void PostTrigger(Entity entity);
+        void PostCrush(Entity entity, Entity enemy);
+    }
+    public abstract class CartBehaviour : EntityBehaviourDefinition, ICartBehaviour
     {
         protected CartBehaviour(string nsp, string name) : base(nsp, name)
         {
         }
         public virtual void PostTrigger(Entity entity)
+        {
+        }
+        public virtual void PostCrush(Entity cart, Entity enemy)
         {
         }
     }
