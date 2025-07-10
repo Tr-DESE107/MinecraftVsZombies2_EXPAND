fdc27d675e7fd980a0628252c4994055e3fb06bb
Cuerzor58 <773785114@qq.com>
2025-07-10 14:08:52 +0800

    炮弹僵尸在死亡时不再具有影子。

--- Assets/Scripts/Vanilla/GameContent/Enemies/Chapter5/CannonballZombie.cs
+++ Assets/Scripts/Vanilla/GameContent/Enemies/Chapter5/CannonballZombie.cs
@@ -1,5 +1,6 @@
 ﻿using MVZ2.GameContent.Buffs.Enemies;
 using MVZ2.Vanilla.Enemies;
+using MVZ2.Vanilla.Entities;
 using PVZEngine.Entities;
 using PVZEngine.Level;
 
@@ -16,6 +17,11 @@ namespace MVZ2.GameContent.Enemies
             base.Init(entity);
             var buff = entity.AddBuff<FlyBuff>();
             buff.SetProperty(FlyBuff.PROP_TARGET_HEIGHT, 20f);
+        }
+        protected override void UpdateLogic(Entity entity)
+        {
+            base.UpdateLogic(entity);
+            entity.SetShadowHidden(entity.IsDead);
         }
     }
 }
