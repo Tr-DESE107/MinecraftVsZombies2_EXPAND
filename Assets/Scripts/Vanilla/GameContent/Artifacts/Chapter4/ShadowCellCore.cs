3cf28333768df7daabb6b77579de7cb764446800
Tr-DESE107 <1992679686@qq.com>
2025-07-24 07:19:27 +0800

    加入了暗影细胞核心

--- Assets/Scripts/Vanilla/GameContent/Artifacts/Chapter4/ShadowCellCore.cs
+++ Assets/Scripts/Vanilla/GameContent/Artifacts/Chapter4/ShadowCellCore.cs
@@ -1,17 +1,21 @@
-using System.Collections.Generic;
-using MVZ2.GameContent.Buffs;
-using MVZ2.GameContent.Buffs.SeedPacks;
-using MVZ2.Vanilla.SeedPacks;
+using MVZ2.Vanilla.Audios;
+using MVZ2.Vanilla.Callbacks;
+using MVZ2.Vanilla.Entities;
 using MVZ2Logic;
 using MVZ2Logic.Artifacts;
 using MVZ2Logic.Level;
-using MVZ2Logic.SeedPacks;
-using PVZEngine;
-using PVZEngine.Auras;
-using PVZEngine.Buffs;
 using PVZEngine.Callbacks;
 using PVZEngine.Entities;
-using PVZEngine.SeedPacks;
+using MVZ2.GameContent.Buffs.Enemies;
+using MVZ2.GameContent.Enemies;
+using MVZ2.Vanilla.Entities;
+using MVZ2.Vanilla.Properties;
+using MVZ2Logic.Level;
+using PVZEngine;
+using PVZEngine.Entities;
+using PVZEngine.Level;
+using Tools;
+using UnityEngine;
 
 namespace MVZ2.GameContent.Artifacts
 {
@@ -20,101 +24,64 @@ namespace MVZ2.GameContent.Artifacts
     {
         public ShadowCellCore(string nsp, string name) : base(nsp, name)
         {
-            AddTrigger(LevelCallbacks.POST_LEVEL_START, PostLevelStartCallback);
-            AddTrigger(LevelCallbacks.POST_ENTITY_INIT, PostEntityInitCallback, filter: EntityTypes.PLANT);
-            AddTrigger(LevelCallbacks.POST_ENTITY_REMOVE, PostEntityRemoveCallback, filter: EntityTypes.PLANT);
-            AddAura(new ReduceCostAura());
+            AddTrigger(VanillaLevelCallbacks.PRE_ENEMY_FAINT, PreEnemyFaintCallback, priority: -100);
         }
-        public override void PostUpdate(Artifact artifact)
+        private void PreEnemyFaintCallback(EntityCallbackParams param, CallbackResult result)
         {
-            base.PostUpdate(artifact);
-            artifact.SetGlowing(true);
-        }
-        private void PostLevelStartCallback(LevelCallbackParams param, CallbackResult result)
-        {
-            var level = param.level;
-            foreach (var artifact in level.GetArtifacts())
+            var enemy = param.entity;
+            if (!enemy.IsHostileEntity())
+                return;
+            var level = enemy.Level;
+            var artifacts = level.GetArtifacts();
+            foreach (var artifact in artifacts)
             {
-                if (artifact == null || artifact.Definition != this)
+                if (artifact?.Definition != this)
                     continue;
-                AuraEffect aura = artifact.GetAuraEffect<ReduceCostAura>();
-                if (aura == null)
-                    continue;
-                aura.UpdateAura();
-            }
-        }
-        private void PostEntityInitCallback(EntityCallbackParams param, CallbackResult result)
-        {
-            var contraption = param.entity;
-            var level = contraption.Level;
-            foreach (var artifact in level.GetArtifacts())
-            {
-                if (artifact == null || artifact.Definition != this)
-                    continue;
-                AuraEffect aura = artifact.GetAuraEffect<ReduceCostAura>();
-                if (aura == null)
-                    continue;
-                aura.UpdateAura();
-            }
-        }
-        private void PostEntityRemoveCallback(EntityCallbackParams param, CallbackResult result)
-        {
-            var contraption = param.entity;
-            var level = contraption.Level;
-            foreach (var artifact in level.GetArtifacts())
-            {
-                if (artifact == null || artifact.Definition != this)
-                    continue;
-                AuraEffect aura = artifact.GetAuraEffect<ReduceCostAura>();
-                if (aura == null)
-                    continue;
-                aura.UpdateAura();
-            }
-        }
-        public static readonly NamespaceID ID = VanillaArtifactID.ShadowCellCore;
+                
+                    //enemy.Revive();
+                    //enemy.Health = enemy.GetMaxHealth();
+                    //enemy.Charm(level.Option.LeftFaction);
+                    //result.SetFinalValue(false);
 
-        public class ReduceCostAura : AuraEffectDefinition
-        {
-            public ReduceCostAura()
-            {
-                BuffID = VanillaBuffID.SeedPack.theCreaturesHeartReduceCost;
-                UpdateInterval = 15;
+                    var randomID = GetRandomSkeletonID(enemy.RNG);
+                    var spawnParam = enemy.GetSpawnParams();
+                    spawnParam.SetProperty(EngineEntityProps.FACTION, enemy.GetFaction());
+                    enemy.Spawn(randomID, enemy.Position, spawnParam);
+
+
+
+                    artifact.Highlight();
+                    enemy.PlaySound(VanillaSoundID.revived);
+                    return;
+                
             }
-            public override void GetAuraTargets(AuraEffect auraEffect, List<IBuffTarget> results)
-            {
-                var level = auraEffect.Source.GetLevel();
-                foreach (var seed in level.GetAllSeedPacks())
-                {
-                    var seedDef = seed?.Definition;
-                    if (seedDef == null)
-                        continue;
-                    if (seedDef.GetSeedType() != SeedTypes.ENTITY)
-                        continue;
-                    var entityID = seedDef?.GetSeedEntityID();
-                    if (!NamespaceID.IsValid(entityID))
-                        continue;
-                    var entityDef = level.Content.GetEntityDefinition(entityID);
-                    if (entityDef == null || entityDef.Type != EntityTypes.PLANT)
-                        continue;
-                    results.Add(seed);
-                }
-            }
-            public override void UpdateTargetBuff(AuraEffect effect, IBuffTarget target, Buff buff)
-            {
-                base.UpdateTargetBuff(effect, target, buff);
-                if (target is not SeedPack seed)
-                    return;
-                var seedDef = seed?.Definition;
-                if (seedDef == null)
-                    return;
-                if (seedDef.GetSeedType() != SeedTypes.ENTITY)
-                    return;
-                var entityID = seed?.Definition?.GetSeedEntityID();
-                if (!NamespaceID.IsValid(entityID))
-                    return;
-                buff.SetProperty(TheCreaturesHeartReduceCostBuff.PROP_ADDITION, seed.Level.GetEntityCount(entityID) * REDUCTION);
-            }
-            public const float REDUCTION = -5;
         }
+
+        public NamespaceID GetRandomSkeletonID(RandomGenerator rng)
+        {
+            var index = rng.WeightedRandom(RandomSkeletonWeights);
+            return RandomSkeleton[index];
+        }
+
+        private static NamespaceID[] RandomSkeleton = new NamespaceID[]
+        {
+            //¹ÖÎï³ö¹Ö
+            VanillaEnemyID.SkeletonHead,
+            VanillaEnemyID.ZombieHead,
+            VanillaEnemyID.RedEyeZombieHead,
+            VanillaEnemyID.HostHead,
+            VanillaEnemyID.dullahanHead,
+        };
+
+        private static int[] RandomSkeletonWeights = new int[]
+        {
+            100,
+            20,
+            1,
+            5,
+            50
+        };
+
+        
     }
 }
