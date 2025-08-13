63fd18e2fda264275519dd8d24d77019bf798175
Tr-DESE107 <1992679686@qq.com>
2025-08-13 18:34:14 +0800

    加入了骷髅头王

--- Assets/Scripts/Vanilla/GameContent/Enemies/VanillaEnemyID.cs
+++ Assets/Scripts/Vanilla/GameContent/Enemies/VanillaEnemyID.cs
@@ -57,6 +57,7 @@ namespace MVZ2.GameContent.Enemies
         public const string NetherVanguard = "NetherVanguard";
         public const string AngryReverser = "AngryReverser";
         public const string RaiderSkull = "RaiderSkull";
+        public const string Anubiskull = "Anubiskull";
 
         public const string reflectiveBarrierZombie = "reflective_barrier_zombie";
         public const string talismanZombie = "talisman_zombie";
@@ -145,6 +146,7 @@ namespace MVZ2.GameContent.Enemies
         public static readonly NamespaceID NetherVanguard = Get(VanillaEnemyNames.NetherVanguard);
         public static readonly NamespaceID AngryReverser = Get(VanillaEnemyNames.AngryReverser);
         public static readonly NamespaceID RaiderSkull = Get(VanillaEnemyNames.RaiderSkull);
+        public static readonly NamespaceID Anubiskull = Get(VanillaEnemyNames.Anubiskull);
 
         public static readonly NamespaceID MonkZombie = Get(VanillaEnemyNames.MonkZombie);
         public static readonly NamespaceID LeatherMonkZombie = Get(VanillaEnemyNames.LeatherMonkZombie);
