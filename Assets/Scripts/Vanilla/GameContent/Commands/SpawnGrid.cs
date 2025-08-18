4c8c2bc43aaa72a7d7cfff9056f4ec7039db304d
Cuerzor58 <773785114@qq.com>
2025-08-18 15:16:26 +0800

    加入spawngrid命令。

---
+++ Assets/Scripts/Vanilla/GameContent/Commands/SpawnGrid.cs
@@ -0,0 +1,40 @@
+﻿using MVZ2.Vanilla;
+using MVZ2Logic;
+using MVZ2Logic.IZombie;
+using PVZEngine;
+using UnityEngine;
+
+namespace MVZ2.GameContent.Commands
+{
+    [CommandDefinition(VanillaCommandNames.spawngrid)]
+    public class SpawnGrid : CommandDefinition
+    {
+        public SpawnGrid(string nsp, string name) : base(nsp, name)
+        {
+        }
+        public override void Invoke(string[] parameters)
+        {
+            var game = Global.Game;
+            var level = Global.Game.GetLevel();
+
+            var id = NamespaceID.Parse(parameters[0], VanillaMod.spaceName);
+
+            var column = Mathf.FloorToInt(level.GetMaxColumnCount() * 0.5f);
+            var lane = Mathf.FloorToInt(level.GetMaxLaneCount() * 0.5f);
+
+            if (parameters.Length >= 2)
+            {
+                column = ParseHelper.ParseInt(parameters[1]);
+            }
+            if (parameters.Length >= 3)
+            {
+                lane = ParseHelper.ParseInt(parameters[2]);
+            }
+
+            var x = level.GetEntityColumnX(column);
+            var z = level.GetEntityLaneZ(lane);
+            var y = level.GetGroundY(x, z);
+            level.Spawn(id, new Vector3(x, y, z), null);
+        }
+    }
+}
--- Assets/Scripts/Vanilla/GameContent/Commands/VanillaCommandID.cs
+++ Assets/Scripts/Vanilla/GameContent/Commands/VanillaCommandID.cs
@@ -7,11 +7,13 @@ namespace MVZ2.GameContent.Commands
     {
         public const string help = "help";
         public const string spawn = "spawn";
+        public const string spawngrid = "spawngrid";
     }
     public static class VanillaCommandID
     {
         public static readonly NamespaceID help = Get(VanillaCommandNames.help);
         public static readonly NamespaceID spawn = Get(VanillaCommandNames.spawn);
+        public static readonly NamespaceID spawngrid = Get(VanillaCommandNames.spawngrid);
         private static NamespaceID Get(string name)
         {
             return new NamespaceID(VanillaMod.spaceName, name);
