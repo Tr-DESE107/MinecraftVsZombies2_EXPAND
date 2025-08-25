d213198d979ced10be601be515621bfd179d3806
Tr-DESE107 <1992679686@qq.com>
2025-08-25 15:49:46 +0800

    完成了无头强袭骑士的代码、完成了狂战士头的代码

--- Assets/Scripts/Vanilla/GameContent/Enemies/VanillaEnemyID.cs
+++ Assets/Scripts/Vanilla/GameContent/Enemies/VanillaEnemyID.cs
@@ -59,6 +59,8 @@ namespace MVZ2.GameContent.Enemies
         public const string RaiderSkull = "RaiderSkull";
         public const string Anubiskull = "Anubiskull";
         public const string WitherSkeletonHorse = "WitherSkeletonHorse";
+        public const string AssaultDullahan = "AssaultDullahan";
+        public const string BerserkerHead = "BerserkerHead";
 
         public const string reflectiveBarrierZombie = "reflective_barrier_zombie";
         public const string talismanZombie = "talisman_zombie";
@@ -149,6 +151,8 @@ namespace MVZ2.GameContent.Enemies
         public static readonly NamespaceID RaiderSkull = Get(VanillaEnemyNames.RaiderSkull);
         public static readonly NamespaceID Anubiskull = Get(VanillaEnemyNames.Anubiskull);
         public static readonly NamespaceID WitherSkeletonHorse = Get(VanillaEnemyNames.WitherSkeletonHorse);
+        public static readonly NamespaceID AssaultDullahan = Get(VanillaEnemyNames.AssaultDullahan);
+        public static readonly NamespaceID BerserkerHead = Get(VanillaEnemyNames.BerserkerHead);
 
         public static readonly NamespaceID MonkZombie = Get(VanillaEnemyNames.MonkZombie);
         public static readonly NamespaceID LeatherMonkZombie = Get(VanillaEnemyNames.LeatherMonkZombie);
