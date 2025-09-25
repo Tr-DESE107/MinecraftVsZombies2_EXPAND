b256766a6de08b3d7139ae704e9c01fa29bc4958
Cuerzor58 <773785114@qq.com>
2025-09-25 12:33:17 +0800

    refactor(contraptions): 梦境结晶的治疗量从40/秒提升至60/秒。

--- Assets/Scripts/Vanilla/GameContent/Contraptions/Chapter2/DreamCrystal.cs
+++ Assets/Scripts/Vanilla/GameContent/Contraptions/Chapter2/DreamCrystal.cs
@@ -38,6 +38,6 @@ namespace MVZ2.GameContent.Contraptions
             contraption.AddBuff<DreamCrystalEvocationBuff>();
             contraption.PlaySound(VanillaSoundID.sparkle);
         }
-        public const float HEAL_PER_FRAME = 1.3333333333f;
+        public const float HEAL_PER_FRAME = 2;
     }
 }
