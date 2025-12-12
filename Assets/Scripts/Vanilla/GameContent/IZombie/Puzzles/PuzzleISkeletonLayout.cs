615f4d6eb394704c70311e5d354ea81019e93443
Tr-DESE107 <1992679686@qq.com>
2025-12-12 23:15:06 +0800

    feat: 修改了：我是僵尸、我是骷髅、牢不可破 三个解谜关卡

--- Assets/Scripts/Vanilla/GameContent/IZombie/Puzzles/PuzzleISkeletonLayout.cs
+++ Assets/Scripts/Vanilla/GameContent/IZombie/Puzzles/PuzzleISkeletonLayout.cs
@@ -13,22 +13,33 @@ namespace MVZ2.GameContent.IZombie
     [IZombieLayoutDefinition(VanillaIZombieLayoutNames.puzzleISkeleton)]
     public class PuzzleISkeletonLayout : IZombieLayoutDefinition
     {
-        public PuzzleISkeletonLayout(string nsp, string name) : base(nsp, name, 4)
+        public PuzzleISkeletonLayout(string nsp, string name) : base(nsp, name, 6)
         {
             Blueprints = new NamespaceID[]
             {
                 VanillaEnemyID.skeleton,
                 VanillaEnemyID.ghost,
-                VanillaEnemyID.necromancer
+                VanillaEnemyID.necromancer,
+                VanillaEnemyID.boneWall,
+                VanillaEnemyID.WitherBoneWall,
+                VanillaEnemyID.MeleeSkeleton,
+                VanillaEnemyID.WitherSkeleton,
+                VanillaEnemyID.IronWitherSkeleton,
+                VanillaEnemyID.BerserkerHead,
+                VanillaEnemyID.skeletonMage,
             };
         }
         public override void Fill(IIZombieMap map, RandomGenerator rng)
         {
-            Insert(map, 2, 2, VanillaContraptionID.glowstone);
-            RandomFill(map, VanillaContraptionID.punchton, 5, rng);
+            Insert(map, 2, 2, VanillaContraptionID.smallDispenser);
+            RandomFill(map, VanillaContraptionID.punchton, 4, rng);
             RandomFill(map, VanillaContraptionID.silvenser, 3, rng);
             RandomFill(map, VanillaContraptionID.dispenser, 3, rng);
-            RandomFill(map, VanillaContraptionID.furnace, 8, rng);
+            RandomFill(map, VanillaContraptionID.furnace, 9, rng);
+            RandomFill(map, VanillaContraptionID.hellfire, 3, rng);
+            RandomFill(map, VanillaContraptionID.totenser, 4, rng);
+            RandomFill(map, VanillaContraptionID.DispenShield, 2, rng);
+            RandomFill(map, VanillaContraptionID.smallDispenser, 2, rng);
         }
     }
 }
