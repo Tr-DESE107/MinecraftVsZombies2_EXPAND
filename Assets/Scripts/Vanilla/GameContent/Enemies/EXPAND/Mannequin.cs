35a88025396a4129a30e7d787d2cb49f785963be
Tr-DESE107 <1992679686@qq.com>
2026-01-09 16:55:26 +0800

    feat: 使得敌方史蒂夫无法召唤部分器械

--- Assets/Scripts/Vanilla/GameContent/Enemies/EXPAND/Mannequin.cs
+++ Assets/Scripts/Vanilla/GameContent/Enemies/EXPAND/Mannequin.cs
@@ -86,6 +86,10 @@ namespace MVZ2.GameContent.Enemies
                     if (validContraptions.Count() <= 0)
                         return;
                     var contraptionID = validContraptions.Random(rng);
+                    if ((contraptionID == VanillaContraptionID.devourer)|| (contraptionID == VanillaContraptionID.jeweledPagoda))
+                    {
+                        contraptionID = VanillaContraptionID.dispenser;
+                    }
                     var spawned = entity.SpawnWithParams(contraptionID, entity.Position);
                     if (spawned != null && spawned.HasBuff<NocturnalBuff>())
                     {
