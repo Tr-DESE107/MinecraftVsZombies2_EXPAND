a2411e864d2329cedd0e4faf8c522c361332e672
Tr-DESE107 <1992679686@qq.com>
2026-01-17 16:38:48 +0800

    feat: 使得购买exp大礼包的钱数上限进一步提升，并修改了unlock里的EXPAND_Completed条件

--- Assets/Scripts/Vanilla/Saves/VanillaSaveData.cs
+++ Assets/Scripts/Vanilla/Saves/VanillaSaveData.cs
@@ -44,7 +44,12 @@ namespace MVZ2.Vanilla.Saves
         }
         public void SetMoney(int value)
         {
-            money = Math.Clamp(value, 0, 1071070);
+            int maxMoney = 1071070;
+            if (IsUnlocked("goods.EXPAND_Completed"))
+            {
+                maxMoney = 99999990;
+            }
+            money = Math.Clamp(value, 0, maxMoney);
         }
         public int GetBlueprintSlots()
         {
