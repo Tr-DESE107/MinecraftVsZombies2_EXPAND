576d75ca284917f32094376df01e78087497816f
Cuerzor58 <773785114@qq.com>
2026-04-24 18:04:47 +0800

    feat: 加入了粉红凋灵。

--- Assets/Scripts/Vanilla/GameContent/Bosses/VanillaBossID.cs
+++ Assets/Scripts/Vanilla/GameContent/Bosses/VanillaBossID.cs
@@ -16,6 +16,7 @@ namespace MVZ2.GameContent.Bosses
         public const string theGiantSnakeTail = "the_giant_snake_tail";
         public const string redDragon = "red_dragon";
         public const string lockedChest = "locked_chest";
+        public const string pinkWither = "pink_wither";
     }
     public static class VanillaBossID
     {
@@ -28,6 +29,7 @@ namespace MVZ2.GameContent.Bosses
         public static readonly NamespaceID theGiantSnakeTail = Get(VanillaBossNames.theGiantSnakeTail);
         public static readonly NamespaceID redDragon = Get(VanillaBossNames.redDragon);
         public static readonly NamespaceID lockedChest = Get(VanillaBossNames.lockedChest);
+        public static readonly NamespaceID pinkWither = Get(VanillaBossNames.pinkWither);
         private static NamespaceID Get(string name)
         {
             return new NamespaceID(VanillaMod.spaceName, name);
