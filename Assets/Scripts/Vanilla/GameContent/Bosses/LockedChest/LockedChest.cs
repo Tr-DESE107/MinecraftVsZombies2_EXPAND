cd7de272cba28f4cbf3a3d4bc123cfb8f1de6558
Cuerzor58 <773785114@qq.com>
2026-04-27 10:11:36 +0800

    balance: 上锁箱子的吐垃圾伤害从50降低至35。

--- Assets/Scripts/Vanilla/GameContent/Bosses/LockedChest/LockedChest.cs
+++ Assets/Scripts/Vanilla/GameContent/Bosses/LockedChest/LockedChest.cs
@@ -216,7 +216,7 @@ namespace MVZ2.GameContent.Bosses
         public const int MAX_REVIVE_TIMES = 3;
         public const float REVIVAL_HEALTH_PERCENTAGE = 0.1f;
         public const float FIRE_BREATH_DAMAGE_MULTIPLIER = 0.01f;
-        public const float SPIT_TRASH_DAMAGE_MULTIPLIER = 0.5f;
+        public const float SPIT_TRASH_DAMAGE_MULTIPLIER = 0.35f;
         public const float EXPLOSIVE_SOUL_MULTIPLIER = 1f;
         public const float BOMBARD_DAMAGE_MULTIPLIER = 18f;
         public const float HYPERBEAM_SCALE = 1f;
