58a5f105865f4493e434f01b641af0233832dd9c
Tr-DESE107 <1992679686@qq.com>
2026-06-02 01:46:29 +0800

    feat: 改进了融合机制，加入多对多模式

--- Assets/Scripts/Vanilla/Entities/Properties/VanillaEntityProps.cs
+++ Assets/Scripts/Vanilla/Entities/Properties/VanillaEntityProps.cs
@@ -610,7 +610,7 @@ namespace MVZ2.Vanilla.Entities
         }
         #endregion
 
-        #region 融合  
+        #region 融合    
         private static PropertyMeta<T> Get<T>(string name, T? defaultValue = default, params string[] obsoleteNames)
         {
             return new VanillaEntityPropertyMeta<T>(name, defaultValue, obsoleteNames);
@@ -626,6 +626,12 @@ namespace MVZ2.Vanilla.Entities
         public static NamespaceID? GetFusionResult(this EntityDefinition entity)
         {
             return entity.GetProperty<NamespaceID>(FUSION_RESULT);
+        }
+
+        public static readonly PropertyMeta<NamespaceID[]> FUSION_RESULTS = Get<NamespaceID[]>("fusionResults");
+        public static NamespaceID[]? GetFusionResults(this EntityDefinition entity)
+        {
+            return entity.GetProperty<NamespaceID[]>(FUSION_RESULTS);
         }
         #endregion
     }
